a12746779a4f3acde1bbd5dc39a88f477bcc14da
fedeatanasoff <fede.atanasoff@gmail.com>
2019-05-05 18:19:06 -0300

    agregando underscore para filtrar los campos a editar con put

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
+const _ = require("underscore");
 const app = express();
 const Usuario = require("../models/Usuario");
 
@@ -30,6 +31,30 @@ app.post("/usuario", (req, res) => {
       data: data
     });
   });
+});
+
+app.put("/usuario/:id", (req, res) => {
+  const id = req.params.id;
+  const body = _.pick(req.body, ["nombre", "email", "img", "role", "estado"]);
+
+  Usuario.findOneAndUpdate(
+    id,
+    body,
+    { new: true, useFindAndModify: false },
+    (err, usuarioDB) => {
+      if (err) {
+        return res.status(400).json({
+          ok: false,
+          err
+        });
+      }
+
+      res.json({
+        ok: true,
+        usuario: usuarioDB
+      });
+    }
+  );
 });
 
 module.exports = app;
