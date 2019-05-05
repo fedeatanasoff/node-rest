6f609bfbf87fcadbac643d6b2491d50d5643aa5e
fedeatanasoff <fede.atanasoff@gmail.com>
2019-05-05 00:56:12 -0300

    mongoose instalado. conexion a la bd ok

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 require("./config");
 const express = require("express");
+const mongoose = require("mongoose");
+const colors = require("colors");
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
@@ -19,6 +21,16 @@ app.post("/", (req, res) => {
   }
 });
 
+mongoose.connect(
+  "mongodb://localhost:27017/cafe",
+  { useNewUrlParser: true },
+  (err, res) => {
+    if (err) throw err;
+
+    console.log("Conexion a la base de datos establecida".bold.green);
+  }
+);
+
 app.listen(process.env.PORT, () =>
-  console.log(`corriendo en puerto ${process.env.PORT}`)
+  console.log(`Servidor corriendo en puerto ${process.env.PORT}`.bold.green)
 );
