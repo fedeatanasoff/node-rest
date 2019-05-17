0ab7b660ace77000e7f64f50b2a7b9e0cd321b9f
fedeatanasoff <fede.atanasoff@gmail.com>
2019-05-16 21:05:51 -0300

    variable de entorno mongo

--- server/config.js
+++ server/config.js
@@ -7,9 +7,9 @@ process.env.NODE_ENV = process.env.NODE_ENV || "dev";
 // BD
 let urlDB;
 
-// if (process.env.NODE_ENV === "dev") {
-//   urlDB = "mongodb://localhost:27017/coffe";
-// } else {
-urlDB = "mongodb://admin:admin1@ds023042.mlab.com:23042/coffeeok";
-// }
+if (process.env.NODE_ENV === "dev") {
+  urlDB = "mongodb://localhost:27017/coffe";
+} else {
+  urlDB = process.env.MONGO_URL;
+}
 process.env.URLDB = urlDB;
